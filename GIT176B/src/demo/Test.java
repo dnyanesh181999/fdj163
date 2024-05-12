ea51c17c90f3cf873161d57b5f4c5e54975fbae9
Dnyanesh Deshmukh <dnyaneshdeshmukh007@gmail.com>
2024-05-12 15:33:39 +0530

    by cjc

--- GIT176B/src/demo/Test.java
+++ GIT176B/src/demo/Test.java
@@ -1,6 +1,9 @@
 package demo;
 
 public class Test {
+	public void m1() {
+		System.out.println("This is m1 method");
+	}
 public static void main(String[] args) {
 	System.out.println("First GIT project");
 }
